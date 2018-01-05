4e026013e5766b0e4e4006ae8e53e182a7f49d9e
konradx <konrad.zwoinski@gmail.com>
2018-01-05 14:59:39 +0100

    basic links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "invoices#index"
   get "invoices" => "invoices#index"
-  get "invoices/:id" => "invoices#show"
+  get "invoices/:id" => "invoices#show", as: "invoice"
 end
